fb73ec2e8abc53ef5456e7b97d12e244c5d382f0
peratop <p.leonardo7k@gmail.com>
2024-10-23 21:34:13 -0300

    Update Main.java
    Adicionado os metodos para a classe Recepcionista.

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,15 @@ class Paciente extends Pessoa {
 }
 
 class Recepcionista extends Pessoa {
+    private int salario;
 
+    public Recepcionista (String nome, String CPF, int idade, int salario) {
+        super(nome, CPF, idade);
+        this.salario = 2200;
+    }
+    public void agendarConsulta(){
+        System.out.println("Agendou uma consulta para o paciente");
+    }
 }
 
 public class Main {
