2286949505da5c9dc2bdbb01bfd2b5c702a47d88
Alex <alexstavasz@gmail.com>
2024-10-23 21:43:40 -0300

    Atualização na classe abstrata Pessoa
    Adicionado atributos, método abstrato, get e set.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,41 @@ class Hospital {
 }
 
 abstract class Pessoa {
+    protected String CPF;
+    protected String nome;
+    protected int idade;
 
+    public Pessoa(int CPF, String nome, int idade) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public abstract void falar();
 }
 
 class Medico extends Pessoa {
