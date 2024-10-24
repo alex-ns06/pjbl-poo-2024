5226e4e7909988e1d365fc593eb6772eee620d41
gxot <gustschott@gmail.com>
2024-10-23 21:00:43 -0300

    Criação de 5 classes, sendo 1 abstrata

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+class Hospital {
+
+}
+
+abstract class Pessoa {
+
+}
+
+class Medico extends Pessoa {
+
+}
+
+class Paciente extends Pessoa {
+
+}
+
+class Recepcionista extends Pessoa {
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
