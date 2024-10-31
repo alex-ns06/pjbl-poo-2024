5d46a79d84ddf8e4832ad2b1d035b6167004f060
gxot <gustschott@gmail.com>
2024-10-30 21:41:06 -0300

    Gustavo 30/10 - Alterações nas classes e correções de erros

--- PJBL/src/Main.java
+++ PJBL/src/Main.java
@@ -1,8 +1,12 @@
-class Hospital {
-    private list<Medico> medicos;
-    private list<Paciente> pacientes;
+import java.util.ArrayList;
 
-    public Hospital () {
+class Hospital {
+    private ArrayList<Pessoa.Medico> medicos = new ArrayList<>();
+    private ArrayList<Pessoa.Paciente> pacientes = new ArrayList<>();
+    private ArrayList<Pessoa.Recepcionista> recepcionistas = new ArrayList<>();
+    String nome;
+
+    public Hospital() {
 
     }
 }
@@ -18,98 +22,71 @@ abstract class Pessoa {
         this.idade = idade;
     }
 
-    public String getCPF() {
-        return CPF;
+
+    public String getCPF() { return CPF; }
+    public void setCPF(String CPF) { this.CPF = CPF; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public int getIdade() { return idade; }
+    public void setIdade(int idade) { this.idade = idade; }
+
+
+    class Medico extends Pessoa {
+        private String especialidade;
+        private String CRM;
+
+        public Medico(String CPF, String nome, int idade, String especialidade, double salario, String CRM) {
+            super(CPF, nome, idade);
+            this.especialidade = especialidade;
+            this.CRM = CRM;
+        }
+
+        public String getEspecialidade() { return especialidade; }
+        public void setEspecialidade(String especialidade) { this.especialidade = especialidade; }
+        public String getCRM() { return CRM; }
+        public void setCRM(String CRM) { this.CRM = CRM; }
+
+        public void mostrarInformacoes() {
+            System.out.println("Médico: " + nome + ", Idade: " + idade + ", Especialidade: " + especialidade);
+        }
+
+        @Override
+        public String toString() {
+            return "Médico " + nome + " (" + especialidade + ") - Salário: ";
+        }
     }
 
-    public String getNome() {
-        return nome;
+
+    class Paciente extends Pessoa {
+        private String plano;
+
+        public Paciente(String CPF, String nome, int idade, String plano) {
+            super(CPF, nome, idade);
+            this.plano = plano;
+        }
+        public String getPlano() { return plano; }
+        public void setPlano(String plano) { this.plano = plano; }
+
+        public void sentirDor() {
+            System.out.println("AI AI!");
+        }
     }
 
-    public int getIdade() {
-        return idade;
-    }
 
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
+    class Recepcionista extends Pessoa {
+        private double salario;
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+        public Recepcionista(String CPF, String nome, int idade, double salario) {
+            super(CPF, nome, idade);
+            this.salario = salario;
+        }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+        public double getSalario() { return salario; }
+        public void setSalario(double salario) { this.salario = salario; }
 
-    public abstract void falar();
-}
-
-class Medico extends Pessoa {
-    private String especialidade;
-    private double salario;
-    private String CRM;
-
-    public Medico(String CPF, String nome, int idade, String especialidades, double salario, String CRM) {
-        super(CPF, nome, idade);
-        this.especialidade = especialidade;
-        this.salario = salario;
-        this.CRM = CRM;
-    }
-
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade {
-        this.especialidade = especialidade
-    }
-
-    public double getSalario {
-        return salario;
-    }
-
-    public void setSalario {
-        this.salario = salario;
-    }
-
-    public String getCRM() {
-        return CRM;
-    }
-
-    public void setCRM() {
-        this.CRM = CRM;
-    }
-
-    public void mostrarInformacoes() {
-        System.out.println("Médico: " + nome + ", Idade: " + idade + ", Especialidade: " + especialidade + ", Salário: " + salario);
-    }
-
-    public String toString() {
-        return "Médico " + nome + " (" + especialidade + ")" + "= Salário: " + salario;
-    }
-}
-
-class Paciente extends Pessoa {
-    String plano;
-    public Paciente (String nome, String CPF, int idade, String plano){
-        super (nome, CPF, idade);
-        this.plano = plano;
-    }
-    public sentirDor (){
-        System.out.println("AI AI!");
-    }
-}
-
-class Recepcionista extends Pessoa {
-    private double salario;
-
-    public Recepcionista (String nome, String CPF, int idade, int salario) {
-        super(nome, CPF, idade);
-        this.salario = 2200.00;
-    }
-    public void agendarConsulta(){
-        System.out.println("Agendou uma consulta para o paciente");
+        public void agendarConsulta() {
+            System.out.println("Agendou uma consulta para o paciente.");
+        }
     }
 }
 
